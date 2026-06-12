6ca8fd2c24910c98534c1960f51933ef2cd5f2f6
aysuhrawat <ayushrawat2101@gmail.com>
2026-06-12 20:07:02 +0530

    pattern

---
+++ basics/pattern12.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class pattern12{
+    public static void main(String[] args ){
+        int n ;
+        Scanner sc = new Scanner ( System.in);
+        System.out.println("enter the value of n ");
+       print( sc.nextInt());
+    }
+    public static void print(int n ){
+         int star =2*(n-1);
+        for(int i = 1;i<=n;i++){
+            for(int j =1;j<=i;j++){
+                System.out.print(j);
+            }
+            for(int k =1;k<=star;k++){
+                System.out.print(" ");
+            }
+            for(int j =i;j>=1;j--){
+                System.out.print(j);
+            }
+            star=star-2;
+            System.out.println(" ");
+    }
+}
+}
---
+++ basics/pattern19.java
@@ -0,0 +1,14 @@
+public class pattern19 {
+    public static void main(String[] args ){
+        for(int i =0 ;i<5;i++){
+            int a =69-i;
+           for(int j =0;j<=i;j++){
+            char ch = (char)a;
+System.out.print(ch);
+a++;
+            }
+            System.out.println(" ");
+        }
+    
+    }
+}
